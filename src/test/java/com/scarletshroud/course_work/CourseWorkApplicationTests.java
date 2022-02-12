e092498d5100aea2f45758da54be79ba0a38d65c
slayer404 <doomer888@protonmail.com>
2022-02-12 22:38:41 +0300

    test

---
+++ src/test/java/com/scarletshroud/course_work/CourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarletshroud.course_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
